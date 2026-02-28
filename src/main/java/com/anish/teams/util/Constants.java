c0bd1001c861ae57d6a8decc4c1da7842c6fd946
anish.anantharaman <anish.anantharaman@tarento.com>
2026-02-28 21:58:23 +0530

    feat: add teams channel notifications with webhook

---
+++ src/main/java/com/anish/teams/util/Constants.java
@@ -0,0 +1,45 @@
+package com.anish.teams.util;
+
+public class Constants {
+
+    private Constants() { }
+
+    public static final class TeamsConstants {
+
+        private TeamsConstants() { }
+
+        public static final String TEXT_BLOCK = "TextBlock";
+        public static final String CONTENT_TYPE_VALUE = "application/vnd.microsoft.card.adaptive";
+        public static final String ADAPTIVE_CARD = "AdaptiveCard";
+        public static final String ACTION_TYPE_VALUE = "Action.OpenUrl";
+        public static final String LARGE = "Large";
+        public static final String EXTRA_LARGE = "ExtraLarge";
+        public static final String MEDIUM = "Medium";
+        public static final String BOLDER = "Bolder";
+
+    }
+
+    public static final class CommonConstants {
+
+        private CommonConstants() { }
+
+        public static final String CONTENT = "content";
+        public static final String CONTENT_TYPE = "contentType";
+        public static final String TYPE = "type";
+        public static final String BODY = "body";
+        public static final String ACTIONS = "actions";
+        public static final String REQUEST_ID_HEADER = "X-Request-Id";
+        public static final String REQUEST_ID = "requestId";
+        public static final String MESSAGE = "message";
+        public static final String ATTACHMENTS = "attachments";
+        public static final String TITLE = "title";
+        public static final String URL = "url";
+        public static final String TEXT = "text";
+        public static final String SIZE = "size";
+        public static final String WEIGHT = "weight";
+        public static final String WRAP = "wrap";
+        public static final String STYLE = "style";
+        public static final String HEADING = "heading";
+
+    }
+}
